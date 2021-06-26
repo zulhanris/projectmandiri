5bf85be7974cf6f893c9ab511de5d76b4ac2b941
Zulhan Ristantyo <zristantyo@gmail.com>
2021-06-26 10:49:47 +0700

    uji kedua

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'admin',
+    loadChildren: ()=>import('./admin/admin.module').then(mod=>mod.AdminModule)
+  },
+  {
+    path:'public',
+    loadChildren: ()=>import('./public/public.module').then(mod=>mod.PublicModule)
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
